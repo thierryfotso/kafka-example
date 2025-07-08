c70d035cde7dac161ab1fa8418c507a12f8f1075
Thierry FOTSO <fotsonono2003@yahoo.fr>
2025-07-08 11:41:20 +0200

    - use spring to inject topic name - use kafka handler

--- src/main/java/com/example/kafka/kafka_example/Consumer.java
+++ src/main/java/com/example/kafka/kafka_example/Consumer.java
@@ -1,20 +1,23 @@
 package com.example.kafka.kafka_example;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
 import com.example.kafka.kafka_example.dto.MessageDto;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @Service
+@KafkaListener(topics = { "${topics}" }, groupId = "group_id")
 public class Consumer {
 
-	public static final String TOPIC = "test_topic";
-
-	@KafkaListener(topics = TOPIC, groupId = "group_id")
-	public void consume(final ConsumerRecord<String, MessageDto> consumerRecord) {
-		System.out.println("--------------message received:" + consumerRecord);
-		final MessageDto message = consumerRecord.value();
-		System.out.println("Consumed Message" + message);
+	@KafkaHandler
+	public void consume(final MessageDto message, @Header("message-id") final String messageId) {
+		log.info("--------------message id:" + messageId);
+		log.info("--------------message received:" + message);
+		System.out.println("--------------message received:" + message);
 	}
 }
--- src/main/java/com/example/kafka/kafka_example/Producer.java
+++ src/main/java/com/example/kafka/kafka_example/Producer.java
@@ -5,16 +5,21 @@ import java.util.concurrent.CompletableFuture;
 
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
 import com.example.kafka.kafka_example.dto.MessageDto;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @Service
 public class Producer {
 
-	private static final String TOPIC = "test_topic";
+	@Value("${topics}")
+	private String topic;
 
 	@Autowired
 	private KafkaTemplate<String, MessageDto> kafkaTemplate;
@@ -23,12 +28,16 @@ public class Producer {
 		System.out.println("**********message to send:" + content);
 		final UUID key = UUID.randomUUID();
 		final MessageDto payload = new MessageDto(content);
-		final ProducerRecord<String, MessageDto> message = new ProducerRecord<>(TOPIC, key.toString(), payload);
+		final ProducerRecord<String, MessageDto> message = new ProducerRecord<>(topic, key.toString(), payload);
 		message.headers().add("message-id", UUID.randomUUID().toString().getBytes());
 
 		final CompletableFuture<SendResult<String, MessageDto>> sendResult = this.kafkaTemplate.send(message);
 		sendResult.whenComplete((result, ex) -> {
-			System.out.println("send result:" + result);
+			if (ex != null) {
+				log.error("An error occured while sending message:", ex);
+			} else {
+				System.out.println("send result:" + result);
+			}
 		});
 	}
 }
